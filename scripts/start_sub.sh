55dac9a55a8ab23979170426724cb96226e9521a
ananmay3 <ananmay3@gs65>
2019-07-31 10:53:38 -0700

    changed gcs id

--- scripts/start_sub.sh
+++ scripts/start_sub.sh
@@ -3,17 +3,20 @@
 # Make sure Port is accessible by mavros
 sudo chmod 777 /dev/ttyACM0
 
+# Initialize roscore
+roscore &
+
 # Launch mavros script
 roslaunch robosub_2019 mavros.launch &
 
 # Launch either joystick or autonomous control
 
 # Launch the state machine
-rosrun robosub_2019 state_machine.py
+rosrun robosub_2019 state_machine.py &
 
 # Launch RC Override node
-rosrun robosub_2019 mavrosrc.py
+rosrun robosub_2019 mavrosrc.py &
 
-trap 'kill $BGPID; exit' SIGINT SIGTERM
+trap 'kill $BGPID; killall rosmaster; killall roscore; exit' SIGINT SIGTERM
 
 wait
